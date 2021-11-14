26cf3bbbb5e6af80efeba1325459566e4458ef1c
Zanxx <78849151+akmalcoeg19@users.noreply.github.com>
2021-11-14 19:42:29 +0700

    Update sstpp.sh

--- menu-all/sstpp.sh
+++ menu-all/sstpp.sh
@@ -5,17 +5,17 @@ NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
 clear
-echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e "\e[1;32m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[m" | lolcat
 echo -e "           \e[1;31m\e[1;31m═[\e[mPanel SSTP\e[1;31m]═\e[m" 
-echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e "\e[1;32m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[m" | lolcat
 echo -e " 1\e[1;33m)\e[m  Create Account SSTP"
 echo -e " 2\e[1;33m)\e[m  Delete SSTP Account"
 echo -e " 3\e[1;33m)\e[m  Renew SSTP Account"
 echo -e " 4\e[1;33m)\e[m  Check User Login SSTP"
 echo -e ""
-echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e "\e[1;32m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[m" | lolcat
 echo -e " x)   MENU"
-echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e "\e[1;32m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[m" | lolcat
 echo -e ""
 read -p "     Please Input Number  [1-4 or x] :  "  sstpp
 echo -e ""
