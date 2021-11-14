29327b48e1a0051c788bb1ca2d127e966587818c
Zanxx <78849151+akmalcoeg19@users.noreply.github.com>
2021-11-14 20:30:51 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 clear
-echo -e "================================================="| lolcat
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"| lolcat
 echo -e "#        Premium Auto Script by Zanxx           #"| lolcat
-echo -e "#-----------------------------------------------#"| lolcat
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"| lolcat
 echo -e "# For Debian 9 & Debian 10 64 bit               #"| lolcat
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"| lolcat
 echo -e "# For VPS with KVM and VMWare virtualization    #"| lolcat
-echo -e "================================================="| lolcat
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"| lolcat
 echo -e "#                  Thanks To                    #"| lolcat
-echo -e "#-----------------------------------------------#"| lolcat
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"| lolcat
 echo -e "# Allah SWT                                     #"| lolcat
 echo -e "# Horasss                                       #"| lolcat
 echo -e "# Remake by Zanxx                               #"| lolcat
-echo -e "================================================="| lolcat
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"| lolcat
