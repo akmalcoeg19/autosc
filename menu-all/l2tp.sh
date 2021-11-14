908b4c537a5e7861f045668b16fa78de8125cf23
Zanxx <78849151+akmalcoeg19@users.noreply.github.com>
2021-11-14 09:16:30 +0700

    Update l2tp.sh

--- menu-all/l2tp.sh
+++ menu-all/l2tp.sh
@@ -7,7 +7,7 @@ echo "Checking VPS"
 clear
 echo -e ""
 echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
-echo -e "         \e[1;31m\e[1;31m═[\e[mL2TP & PPTP Account\e[1;31m]═\e[m" 
+echo -e "         \e[1;31m\e[1;31m═[\e[mPanel L2TP & PPTP\e[1;31m]═\e[m" 
 echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
 echo -e " 1\e[1;33m)\e[m  Creating L2TP Account"
 echo -e " 2\e[1;33m)\e[m  Create Account PPTP"
